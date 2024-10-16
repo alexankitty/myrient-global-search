50d45b041bddbf1e68b29203e9144bf334e051ef
Alexandra <alexankitty@gmail.com>
2024-10-16 03:09:31 -0600

    idk man I did a ton of stuff. plus prelim categories wooh

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 import getAllFiles from './lib/dirwalk.js'
-import parseJsonFile from './lib/loadfiles.js'
+import {parseJsonFile, saveJsonFile, fileExists} from './lib/loadfiles.js'
 import fuzzySearch from './lib/search.js'
+import cron from 'node-cron'
+import FileOlderThan from 'file-older-than'
 import 'dotenv/config'
 import express from 'express'
 
-var fileList = await parseJsonFile('./filelist.json')
+
+var fileListPath = './filelist.json'
+var fileList = []
+
+async function getFilesJob(){
+  console.log('Updating the file list.')
+  fileList = await getAllFiles()
+  saveJsonFile(fileListPath, fileList)
+  console.log(`Finished updating file list. ${fileList.length} found.`)
+}
+
+if(!fileExists(fileListPath) || FileOlderThan(fileListPath, '1d')){
+  await getFilesJob()
+}
+else{
+  fileList = await parseJsonFile(fileListPath)
+}
 
 var app = express();
 app.set('view engine', 'ejs')
@@ -28,3 +46,5 @@ app.get('/search', function(req, res) {
 app.listen(process.env.PORT)
 console.log(`Listening on ${process.env.PORT}.`)
 console.log(`Loaded ${fileList.length} known files.`)
+
+cron.schedule('0 0 0 * * *', getFilesJob)
